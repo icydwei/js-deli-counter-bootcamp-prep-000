fc750bc6230d0aaee75635f208749568481be64c
Rob Paik <robpaik@gmail.com>
2019-06-17 03:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber (line, customername) {
 
 function nowServing (line) {
   if (line.length > 0) {
-    return line[0];
+    return (`Currently serving ${line[0]}.`)
   }
   else {
     return "There is nobody waiting to be served!"
