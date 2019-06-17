30691ac871a6c66f16e9606b339768e6d4292c16
Rob Paik <robpaik@gmail.com>
2019-06-17 02:48:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (line, customername) {
   line.push(customername);
   var position = line.length
-  console.log(`Weclome, ${customername}. You are number ${position} in line.`)
+  return console.log(`Weclome, ${customername}. You are number ${position} in line.`)
 }
