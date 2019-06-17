f13eb1c6ec43065038d7fd52aa4694e553253f73
Rob Paik <robpaik@gmail.com>
2019-06-17 02:47:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,5 @@
+function takeANumber (line, customername) {
+  line.push(customername);
+  position = line.length
+  console.log(`Weclome, ${customername}. You are number ${position} in line.")
+  }
